d8988ed6fb49c2485d198a05cea0f03cee46fa02
Utkarsh Singh <utkarsh.singh.19@mountblue.tech>
2022-04-06 12:59:35 +0530

    app submission

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as RDC from "react-dom/client"
+import Game from "./Game";
+import "./index.css"
+const root = document.getElementById("root");
+const app = RDC.createRoot(root);
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+app.render(
+    <Game />
+)
